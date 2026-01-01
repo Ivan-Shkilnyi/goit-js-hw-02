5453ccc4bd792c848c2668561f9c01a6650a3173
John <ivan.shkilnyi@gmail.com>
2026-01-02 00:14:18 +0200

    fix > in tasck-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
-    if (customerCredits > totalPrice) {
+    if (totalPrice > customerCredits) {
         return 'Insufficient funds!';
     } else {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
